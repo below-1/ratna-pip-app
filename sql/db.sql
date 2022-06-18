8de44616d4b871f78b43512a27eac52839815536
Below Bed <jordanmeta4677@gmail.com>
2022-06-19 01:38:00 +0800

    add kriteria columns

--- sql/db.sql
+++ sql/db.sql
@@ -15,7 +15,12 @@ create table siswa (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   nisn STRING NOT NULL UNIQUE,
   nama STRING NOT NULL,
+  
   jenis_kelamin jenis_kelamin_type NOT NULL,
+  penghasilan_ortu INTEGER,
+  tanggungan_ortu INTEGER,
+  hafalan_alquran INTEGER,
+  
   kelas integer not null,
   sub_kelas integer not null,
   avatar STRING,
